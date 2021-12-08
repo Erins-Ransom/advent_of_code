d0efae1c3996cbbe908a3ad83e460b7726285e02
Eric Knorr <eric_knorr@g.harvard.edu>
2021-12-08 15:08:33 -0500

    python day 8

---
+++ day_8.py
@@ -0,0 +1,70 @@
+
+###### PART 1 ######
+count = 0
+for output in [line.split('| ')[-1] for line in open("input_8.txt").read().split('\n')]:
+    for string in output.split(' '):
+        if len(string) == 2 or len(string) == 3 or len(string) == 4 or len(string) == 7:
+            count += 1
+print(count)
+
+###### PART 2 ######
+class decoder:
+    def __init__(self, codes_string):
+        codes = [frozenset(code) for code in codes_string.split(' ')]
+        self.f = dict({})
+        self.inv = dict({})
+        # first we decode unique length values: 1,4,7,8
+        for code in codes:
+            if len(code) == 2:
+                self.f[code] = 1
+                self.inv[1] = code
+            elif len(code) == 3:
+                self.f[code] = 7
+                self.inv[7] = code
+            elif len(code) == 4:
+                self.f[code] = 4
+                self.inv[4] = code
+            elif len(code) == 7:
+                self.f[code] = 8
+                self.inv[8] = code
+        # then we decode the remaing values based on the previous
+        while len(self.f) < 10:
+            for code in codes:
+                if len(code) == 5:
+                    if all([x in code for x in self.inv[1]]):
+                        self.f[code] = 3
+                        self.inv[3] = code
+                    elif 9 in self.inv:
+                        if all([x in self.inv[9] for x in code]):
+                            self.f[code] = 5
+                            self.inv[5] = code
+                        else:
+                            self.f[code] = 2
+                            self.inv[2] = code
+                if len(code) == 6:
+                    if all([x in code for x in self.inv[4]]):
+                        self.f[code] = 9
+                        self.inv[9] = code
+                    elif all([x in code for x in self.inv[7]]):
+                        self.f[code] = 0
+                        self.inv[0] = code
+                    else:
+                        self.f[code] = 6
+                        self.inv[6] = code
+
+    def __call__(self, string_code):
+        result = 0
+        power = 0
+        for value in reversed(string_code.split(' ')):
+            result += self.f[frozenset(value)] * 10 ** power
+            power += 1
+        return result
+
+    def inv(self, int_value):
+        return self.inv[int_value]
+
+total = 0
+for codes, output in [(line.split(' | ')[0], line.split(' | ')[-1]) for line in open("input_8.txt").read().split('\n')]:
+    total += decoder(codes)(output)
+
+print(total)
